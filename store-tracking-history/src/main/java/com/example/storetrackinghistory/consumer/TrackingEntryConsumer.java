a362b6765ee04c2edff4edc6c40b4c50346d3d5d
emerson.lopes <emerson.lopes@sspds.ce.gov.br>
2021-12-03 16:26:32 -0300

    Configurado o max poll

--- store-tracking-history/src/main/java/com/example/storetrackinghistory/consumer/TrackingEntryConsumer.java
+++ store-tracking-history/src/main/java/com/example/storetrackinghistory/consumer/TrackingEntryConsumer.java
@@ -5,7 +5,6 @@ import com.example.storetrackinghistory.service.TrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 @Component
 public class TrackingEntryConsumer {
